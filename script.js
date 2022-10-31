b62c8373593c51b67bf3d38d55f2f73835ae647c
Dasago250 <Dasago250@gmail.com>
2022-10-31 11:19:21 -0500

    Add comments and semicolons

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ keyboard.forEach(key => {
       case "extra":
         pointSign(key.getAttribute('id'));
         break;
-      default:
-        break;
     }
   })
 })
@@ -46,7 +44,7 @@ function multiplication(numberA, numberB) {
 
 function division(numberA, numberB) {
   if (numberB == 0) {
-    alert("ERROR you can't divide by zero")
+    alert("ERROR you can't divide by zero");
     return 0;
   }
   return numberA / numberB;
@@ -76,16 +74,16 @@ function operate(numberA, numberB, operation) {
 function operationManager(operation) {
   let result;
   if(screen2.textContent == ""){
-    screen2.textContent = screen1.textContent
-    numberA = +screen2.textContent
-    currentOperation = operation
-    screen1.textContent = ""
+    screen2.textContent = screen1.textContent;
+    numberA = +screen2.textContent;
+    currentOperation = operation;
+    screen1.textContent = "";
   }else{
     numberB = +screen1.textContent;
-    result = operate(numberA,numberB,currentOperation)
+    result = operate(numberA,numberB,currentOperation);
     currentOperation = operation;
     screen2.textContent = checkPoint(result);
-    screen1.textContent = ""
+    screen1.textContent = "";
     numberA = result;
     numberB = 0;
   }
@@ -99,12 +97,13 @@ function equal() {
     result = operate(numberA,numberB,currentOperation)
     currentOperation = "";
     screen2.textContent = checkPoint(result);
-    screen1.textContent = ""
+    screen1.textContent = "";
     numberA = result;
-    numberB = 0
+    numberB = 0;
   }
 }
 
+//Check if the number has already a point
 function checkPoint(number){
   let text = number.toString();
   if (text.includes('.')) {
@@ -113,29 +112,31 @@ function checkPoint(number){
   return number;
 }
 
+//Clears all the screens and variables
 function Clear(key){
   if (key == "CE") {
-    screen1.textContent = ""
+    screen1.textContent = "";
   }else{
-    screen1.textContent = ""
-    screen2.textContent = ""
+    screen1.textContent = "";
+    screen2.textContent = "";
     currentOperation = "";
     numberA = 0;
     numberB = 0;
   }
 }
 
+//Check if point and sign are already displaying
 function pointSign(key){
   if (key == "point") {
     let number = screen1.textContent;
     if (!number.includes('.')) {
-      screen1.textContent += '.'
+      screen1.textContent += '.';
     }
   }
   if (key == "sign") {
     let number = screen1.textContent;
     if (!number.includes('-') && screen1.textContent != "") {
-      screen1.textContent = "-".concat(number)
+      screen1.textContent = "-".concat(number);
     }else if(number.includes('-')){
       screen1.textContent = number.slice(1 , number.length);
     }
